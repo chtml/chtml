115e4da4f60e295387428f579879f876b281f056
浩知 <haozhi.lyj@alibaba-inc.com>
2017-05-04 23:09:43 +0800

    init

---
+++ src/index.js
@@ -0,0 +1,20 @@
+class Init {
+  constructor () {
+    this.foo = 'foo'
+  }
+
+  async run () {
+    console.log('hello')
+    await this.sleep(1)
+    console.log('world')
+  }
+
+  sleep (delay) {
+    return new Promise(resolve => {
+      setTimeout(resolve, delay * 1000)
+    })
+  }
+}
+
+new Init().run()
+
